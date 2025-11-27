e88c444fd0bb560df5e28ff12cdfd707f7117480
Pedro Casseb <casseb.phcc@gmail.com>
2025-11-27 16:01:25 -0300

    implement windows with zustand

--- src/components/Dock.jsx
+++ src/components/Dock.jsx
@@ -3,8 +3,10 @@ import { Tooltip } from 'react-tooltip';
 import { useRef } from 'react'
 import { useGSAP} from '@gsap/react';
 import { gsap } from 'gsap';
+import useWindowStore from '#store/window';
 
 const Dock = () => {
+    const { openWindow, closeWindow, windows } = useWindowStore();
     const dockRef = useRef(null);
 
     useGSAP(() => {
@@ -52,6 +54,20 @@ const Dock = () => {
     }, [])
 
     const toggleApp = (app) => {
+        if(!app.canOpen) return;
+
+        const window =  windows[app.id];
+
+        if(!window) {
+            console.error(`Window with id ${app.id} not found`);
+            return;
+        }
+
+        if(window.isOpen) {
+            closeWindow(app.id);
+        } else {
+            openWindow(app.id);
+        }
     }
 
     return (
